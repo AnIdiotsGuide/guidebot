0ec497e8c82601c29f724b10fc8c9e684619111a
Évelyne Lachance <eslachance@gmail.com>
2017-06-29 12:34:52 -0400

    Create stats.js

---
+++ commands/stats.js
@@ -0,0 +1,28 @@
+const { version } = require("discord.js");
+const moment = require("moment");
+require("moment-duration-format");
+
+exports.run = (client, msg, args) => {
+    const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+    msg.channel.send(`= STATISTICS =
+• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+• Uptime     :: ${duration}
+• Users      :: ${client.users.size.toLocaleString()}
+• Servers    :: ${client.guilds.size.toLocaleString()}
+• Channels   :: ${client.channels.size.toLocaleString()}
+• Discord.js :: v${version}
+• Node       :: ${process.version}`, {code: "asciidoc"});
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'stats',
+  description: 'Gives some useful bot statistics',
+  usage: 'stats'
+};
