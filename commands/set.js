515b12d909c472e351b0a71c7d15b8efb9d5a677
Evelyne Lachance <me@evie.codes>
2017-07-19 13:55:37 -0400

    Consistency for  being added. Also more comments

--- commands/set.js
+++ commands/set.js
@@ -1,14 +1,32 @@
 const { inspect } = require("util");
 
-exports.run = async (client, message, [action, key, ...value]) => {
+// This command is to modify/edit guild configuration. Perm Level 3 for admins
+// and owners only. Used for changing prefixes and role names and such.
+
+// Note that there's no "checks" in this basic version - no config "types" like
+// Role, String, Int, etc... It's basic, to be extended with your deft hands!
+
+// Note the **destructuring** here. instead of `args` we have :
+// [action, key, ...value]
+// This gives us the equivalent of either: 
+// const action = args[0]; const key = args[1]; const value = args.slice(2);
+// OR the same as: 
+// const [action, key, ...value] = args;
+exports.run = async (client, message, [action, key, ...value], level) => { // eslint-disable-line no-unused-vars
+
+  // Retrieve current guild settings
   const settings = client.settings.get(message.guild.id);
 
+  // First, if a user does `-set edit <key> <new value>`, let's change it
   if(action === "edit") {
     if(!key) return message.reply("Please specify a key to edit");
     if(!settings[key]) return message.reply("This key does not exist in the settings");
     if(!value) return message.reply("Please specify a new value");
-
+    
+    // `value` being an array, we need to join it first.
     settings[key] = value.join(" ");
+    
+    // One the settings is modified, we write it back to the collection
     client.settings.set(message.guild.id, settings);
     message.reply(`${key} successfully edited to ${value}`);
   } else
