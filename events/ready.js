e0309ede0f37ad179c8f5905e088c3a30a7fea0d
Évelyne Lachance <eslachance@gmail.com>
2017-08-10 07:40:01 -0400

    Update ready.js
    wait() no longer global

--- events/ready.js
+++ events/ready.js
@@ -2,8 +2,8 @@
 module.exports = async client => {
   // Why await here? Because the ready event isn't actually ready, sometimes
   // guild information will come in *after* ready. 1s is plenty, generally,
-  // for lal of them to be loaded.
-  await wait(1000);
+  // for all of them to be loaded.
+  await client.wait(1000);
 
   // Both `wait` and `client.log` are in `./modules/functions`.
   client.log("log", `Ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "Ready!");
