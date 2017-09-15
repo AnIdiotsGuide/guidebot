e5e6fe576c25f2c88a59e59935bcfe42416cb172
Evelyne Lachance <eslachance@gmail.com>
2017-09-15 09:54:35 -0400

    added reboot command

---
+++ commands/reboot.js
@@ -0,0 +1,27 @@
+let hasPM2;
+
+try {
+  require.resolve("pm2");
+  hasPM2 = "PM2 is installed, hopefully that means this bot will reboot in a moment!";
+} catch (e) {
+  hasPM2 = "Cannot find PM2. You must restart this bot manually from the command prompt.";
+}
+
+exports.run = async (client, message, args, level) => {// eslint-disable-line no-unused-vars
+  await message.reply(`Bot is shutting down. ${hasPM2}`);
+  process.exit(1);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "Bot Admin"
+};
+
+exports.help = {
+  name: "reboot",
+  category: "System",
+  description: "Shuts down the bot. If running under PM2, bot will restart automatically.",
+  usage: "reboot"
+};
