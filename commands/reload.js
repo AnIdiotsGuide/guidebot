e99bc638c4bf6c006bc6cc06b1a44b293ae41381
Évelyne Lachance <eslachance@gmail.com>
2017-10-06 18:15:06 -0400

    Load/Unload commands are now done through a client function

--- commands/reload.js
+++ commands/reload.js
@@ -1,34 +1,13 @@
 exports.run = async (client, message, args, level) => {// eslint-disable-line no-unused-vars
   if (!args || args.size < 1) return message.reply("Must provide a command to reload. Derp.");
 
-  let command;
-  if (client.commands.has(args[0])) {
-    command = client.commands.get(args[0]);
-  } else if (client.aliases.has(args[0])) {
-    command = client.commands.get(client.aliases.get(args[0]));
-  }
-  if (!command) return message.reply(`The command \`${args[0]}\` doesn"t seem to exist, nor is it an alias. Try again!`);
+  let response = await client.unloadCommand(args[0]);
+  if (response) return message.reply(`Error Unloading: ${response}`);
 
-  if (command.shutdown) {
-    await command.shutdown(client);
-  }
+  response = client.loadCommand(args[0]);
+  if (response) return message.reply(`Error Loading: ${response}`);
 
-  const commandName = command.help.name;
-  delete require.cache[require.resolve(`./${commandName}.js`)];
-  const cmd = require(`./${commandName}`);
-  client.commands.delete(cmd.help.name);
-  client.aliases.forEach((cmdName, alias) => {
-    if (cmdName === cmd.help.name) client.aliases.delete(alias);
-  });
-  client.commands.set(cmd.help.name, cmd);
-  if (cmd.init) {
-    cmd.init(client);
-  }
-  cmd.conf.aliases.forEach(alias => {
-    client.aliases.set(alias, cmd.help.name);
-  });
-
-  message.reply(`The command \`${cmd.help.name}\` has been reloaded`);
+  message.reply(`The command \`${args[0]}\` has been reloaded`);
 };
 
 exports.conf = {
