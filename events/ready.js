a2cc8d58897e84c3848d94d6c81fde8a78f5cca5
Nathaniel Chin <thegamingorangutans@gmail.com>
2017-10-30 21:54:03 +0800

    make it look nicer

--- events/ready.js
+++ events/ready.js
@@ -14,6 +14,6 @@ module.exports = async client => {
   client.user.setStatus("dnd");
 
   const { version } = require("discord.js");
-  client.log(`Discord.js :: v${version}
-  Node       :: ${process.version}`);
+  client.log('Discord.js :: v${version}\
+  Node       :: ${process.version}');
 };
