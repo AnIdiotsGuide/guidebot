8bfd37aaf466f6ee56587b4a1bf0bba1739c482e
TaiweiLoh <tweettweettaiwei@gmail.com>
2017-11-11 20:10:09 +1300

    Change it here as well.
    Reload aliases.

--- commands/reload.js
+++ commands/reload.js
@@ -1,13 +1,19 @@
 exports.run = async (client, message, args, level) => {// eslint-disable-line no-unused-vars
-  if (!args || args.size < 1) return message.reply("Must provide a command to reload. Derp.");
+        if (!args || args.size < 1) return message.reply('Must provide a command to reload.');
+        var commands;
+        if (this.client.commands.has(args[0])) {
+            commands = this.client.commands.get(args[0]);
+        } else if (this.client.aliases.has(args[0])) {
+            commands = this.client.commands.get(this.client.aliases.get(args[0]));
+        }
+        if (!commands) return message.reply(`The command \`${args[0]}\` doesn't seem to exist, nor is it an alias. Try again!`);
+        let response = await this.client.unloadCommand(`${commands.conf.location}`, commands.help.name);
+        if (response) return message.reply(`Error Unloading: ${response}`);
 
-  let response = await client.unloadCommand(args[0]);
-  if (response) return message.reply(`Error Unloading: ${response}`);
+        response = this.client.loadCommand(`${commands.conf.location}`, commands.help.name);
+        if (response) return message.reply(`Error loading: ${response}`);
 
-  response = client.loadCommand(args[0]);
-  if (response) return message.reply(`Error Loading: ${response}`);
-
-  message.reply(`The command \`${args[0]}\` has been reloaded`);
+        message.reply(`The command \`${commands.help.name}\` has been reloaded`);
 };
 
 exports.conf = {
