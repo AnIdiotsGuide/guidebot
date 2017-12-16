1637f673f87351e91ab9687ca7a2563ad63d93ca
goatwithakb <leonbohm@gmail.com>
2017-12-16 15:17:45 +0000

    Style consistency

--- background/examplebg.js
+++ background/examplebg.js
@@ -1,19 +1,19 @@
 const slug = "examplebg";
 
-module.exports.conf = {
+exports.conf = {
   slug: slug,
   interval: 3000 // milliseconds
 };
 
-module.exports.init = () => {
+exports.init = () => {
   console.log("Worker has been initialized.");
 };
 
-var ranTimes = 0;
+let ranTimes = 0;
 
-module.exports.run = (client) => {
+exports.run = (client) => {
   console.log(`${slug} worker ran ${++ranTimes} times.`);
-  if (ranTimes == 4) {
+  if (ranTimes === 4) {
     console.log(`${slug} ran 4 times. Unloading..`);
     client.unloadBackgroundWorker(slug);
   }
