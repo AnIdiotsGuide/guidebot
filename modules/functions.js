bf659d324b7893282fad5a320cd7ddf59c23cb99
York <YorkAARGH@users.noreply.github.com>
2018-01-14 12:45:47 +0000

    Update functions.js
    Fixed `awaitReply`, was assigning not comparing author ID's

--- modules/functions.js
+++ modules/functions.js
@@ -38,7 +38,7 @@ module.exports = (client) => {
 
   */
   client.awaitReply = async (msg, question, limit = 60000) => {
-    const filter = m=>m.author.id = msg.author.id;
+    const filter = m => m.author.id === msg.author.id;
     await msg.channel.send(question);
     try {
       const collected = await msg.channel.awaitMessages(filter, { max: 1, time: limit, errors: ["time"] });
