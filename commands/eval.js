d4d1b42e9d9be738969feaf2239c43eed52de181
Soumil07 <soumil.paranjpay@gmail.com>
2018-02-20 21:34:44 +0530

    port eval fix from class

--- commands/eval.js
+++ commands/eval.js
@@ -6,27 +6,36 @@
 
 // However it's, like, super ultra useful for troubleshooting and doing stuff
 // you don't want to put in a command.
-exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  const code = args.join(" ");
-  try {
-    const evaled = eval(code);
-    const clean = await client.clean(client, evaled);
-    message.channel.send(`\`\`\`js\n${clean}\n\`\`\``);
-  } catch (err) {
-    message.channel.send(`\`ERROR\` \`\`\`xl\n${await client.clean(client, err)}\n\`\`\``);
+const Command = require("../base/Command.js");
+
+class Eval extends Command {
+  constructor(client) {
+    super(client, {
+      name: "eval",
+      description: "Evaluates arbitrary Javascript.",
+      category:"System",
+      usage: "eval <expression>",
+      aliases: ["ev"],
+      permLevel: "Bot Owner"
+    });
   }
-};
 
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: "Bot Owner"
-};
+  async run(message, args, level) { // eslint-disable-line no-unused-vars
+    const code = args.join(" ");
+    try {
+      const evaled = eval(code);
+      const clean = await this.client.clean(this.client, evaled);
+      // sends evaled output as a file if it exceeds the maximum character limit
+      // 6 graves, and 2 characters for "js"
+      const MAX_CHARS = 3 + 2 + clean.length + 3;
+      if (MAX_CHARS > 2000) {
+        message.channel.send("Output exceeded 2000 characters. Sending as a file.", { files: [{ attachment: Buffer.from(clean), name: "output.txt" }] });
+      }
+      message.channel.send(`\`\`\`js\n${clean}\n\`\`\``);
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${await this.client.clean(this.client, err)}\n\`\`\``);
+    }
+  }
+}
 
-exports.help = {
-  name: "eval",
-  category: "System",
-  description: "Evaluates arbitrary javascript.",
-  usage: "eval [...code]"
-};
+module.exports = Eval;
