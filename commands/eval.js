8b0273e63ba4e29fd2dcde0aa7295f7b7cbbac82
Soumil07 <soumil.paranjpay@gmail.com>
2018-02-22 20:55:15 +0530

    requested changes

--- commands/eval.js
+++ commands/eval.js
@@ -3,24 +3,37 @@
 // can be used to do **anything** on your machine, from stealing information to
 // purging the hard drive. DO NOT LET ANYONE ELSE USE THIS
 
-
 // However it's, like, super ultra useful for troubleshooting and doing stuff
 // you don't want to put in a command.
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  const code = args.join(" ");
-  try {
-    const evaled = eval(code);
-    const clean = await client.clean(client, evaled);
-    // sends evaled output as a file if it exceeds the maximum character limit
-    // 6 graves, and 2 characters for "js"
-    const MAX_CHARS = 3 + 2 + clean.length + 3;
-    if (MAX_CHARS > 2000) {
-      message.channel.send("Output exceeded 2000 characters. Sending as a file.", { files: [{ attachment: Buffer.from(clean), name: "output.txt" }] });
-    }
-    message.channel.send(`\`\`\`js\n${clean}\n\`\`\``);
-  } catch (err) {
-    message.channel.send(`\`ERROR\` \`\`\`xl\n${await client.clean(client, err)}\n\`\`\``);
+  const { output, error } = this.tryEval(args.join(" "));
+  const cleaned = await client.clean(client, output);
+  // sends output as a file if it exceeds the character limit
+  // note: if the output is an error, it will have the ERROR heading, hence the extra 7 
+  const MAX_CHARS = error ? 3 + 2 + 7 + cleaned.length + 2 + 3 : 3 + 2 + cleaned.length + 2 + 3;
+  if (MAX_CHARS > 2000) {
+    message.channel.send("Output exceeded 2000 characters. Sending as a file.", { files: [{ attachment: Buffer.from(cleaned), name: "output.txt" }] });
   }
+  return message.channel.send(error ? `\`ERROR\` \`\`\`js\n${cleaned}\n\`\`\`` : `\`\`\`js\n${cleaned}\n\`\`\``);
+};
+
+// evals code and returns the output
+// if it evaluates succesfully, it will return the output and "error" as false
+// if it errors, it will return the output as the error and "error" as true
+exports.tryEval = code => {
+  let output, error;
+  try {
+    output = eval(code);
+    error = false;
+  } catch (e) {
+    output = e;
+    error = true;
+  }
+  
+  return {
+    output,
+    error,
+  };
 };
 
 exports.conf = {
