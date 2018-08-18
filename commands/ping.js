bd835fe183767b412b59b6e2b786d37624e2bbcf
Évelyne Lachance <eslachance@gmail.com>
2018-08-18 00:33:24 -0400

    Update to latest discord.js 11.4.2

--- commands/ping.js
+++ commands/ping.js
@@ -13,6 +13,6 @@ exports.conf = {
 exports.help = {
   name: "ping",
   category: "Miscelaneous",
-  description: "It... like... pings. Then Pongs. And it\"s not Ping Pong.",
+  description: "It like... Pings. Then Pongs. And it's not Ping Pong.",
   usage: "ping"
 };
