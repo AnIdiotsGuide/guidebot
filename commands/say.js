52e06108bc7cf8796af51c4d90fb00e3cef9e259
TheBigerGamer <39341546+TheBigerGamer@users.noreply.github.com>
2019-01-22 18:55:58 +0000

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -5,3 +5,17 @@ var newMessage = message.replace("say ", "");
 //Making a variable where "say " is removed 
 bot.sendMessage({to: channelID, message:newMessage})
 //Send the new variable. } break;
+  
+ exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "say",
+  category: "Outros",
+  description: "Faz o bot dizer aquilo que escreves.",
+  usage: "say"
+};
