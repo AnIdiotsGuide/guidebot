69c7d845c3a28dd362946b9d37a177e32531ad68
TheBigerGamer <39341546+TheBigerGamer@users.noreply.github.com>
2019-01-22 18:16:31 +0000

    Update set.js

--- commands/set.js
+++ commands/set.js
@@ -21,14 +21,14 @@ exports.run = async (client, message, [action, key, ...value], level) => { // es
   // Edit an existing key value
   if (action === "edit") {
     // User must specify a key.
-    if (!key) return message.reply("Please specify a key to edit");
+    if (!key) return message.reply("Por favor especifica uma chave para setar.");
     // User must specify a key that actually exists!
-    if (!defaults[key]) return message.reply("This key does not exist in the settings");
+    if (!defaults[key]) return message.reply("Esta chave não existe nas definições!");
     const joinedValue = value.join(" ");
     // User must specify a value to change.
-    if (joinedValue.length < 1) return message.reply("Please specify a new value");
+    if (joinedValue.length < 1) return message.reply("Por favor especifica um novo valor para esta chave.");
     // User must specify a different value than the current one.
-    if (joinedValue === settings[key]) return message.reply("This setting already has that value!");
+    if (joinedValue === settings[key]) return message.reply("Esta definição já tem esse valor!");
     
     // If the guild does not have any overrides, initialize it.
     if (!client.settings.has(message.guild.id)) client.settings.set(message.guild.id, {});
@@ -37,42 +37,42 @@ exports.run = async (client, message, [action, key, ...value], level) => { // es
     client.settings.set(message.guild.id, joinedValue, key);
 
     // Confirm everything is fine!
-    message.reply(`${key} successfully edited to ${joinedValue}`);
+    message.reply(`${key} editado com sucesso para ${joinedValue}`);
   } else
   
   // Resets a key to the default value
   if (action === "del" || action === "reset") {
-    if (!key) return message.reply("Please specify a key to reset.");
-    if (!defaults[key]) return message.reply("This key does not exist in the settings");
-    if (!overrides[key]) return message.reply("This key does not have an override and is already using defaults.");
+    if (!key) return message.reply("Por favor especifica uma chave para resetar.");
+    if (!defaults[key]) return message.reply("Esta chave não existe nas definições!");
+    if (!overrides[key]) return message.reply("Esta chave não tem um override ou já está no modo standart.");
     
     // Good demonstration of the custom awaitReply method in `./modules/functions.js` !
-    const response = await client.awaitReply(message, `Are you sure you want to reset ${key} to the default value?`);
+    const response = await client.awaitReply(message, `Tens a certeza de que quer resetar ${key} para o valor original?`);
 
     // If they respond with y or yes, continue.
-    if (["y", "yes"].includes(response.toLowerCase())) {
+    if (["s", "sim"].includes(response.toLowerCase())) {
       // We delete the `key` here.
       client.settings.delete(message.guild.id, key);
-      message.reply(`${key} was successfully reset to default.`);
+      message.reply(`${key} foi resetada para o modo standart com sucesso!`);
     } else
     // If they respond with n or no, we inform them that the action has been cancelled.
-    if (["n","no","cancel"].includes(response)) {
-      message.reply(`Your setting for \`${key}\` remains at \`${settings[key]}\``);
+    if (["n","não","cancelar"].includes(response)) {
+      message.reply(`A definição para \`${key}\` permanece em \`${settings[key]}\``);
     }
   } else
   
   if (action === "get") {
-    if (!key) return message.reply("Please specify a key to view");
-    if (!defaults[key]) return message.reply("This key does not exist in the settings");
-    const isDefault = !overrides[key] ? "\nThis is the default global default value." : "";
-    message.reply(`The value of ${key} is currently ${settings[key]}${isDefault}`);
+    if (!key) return message.reply("Por favor especifica uma chave para a ver.");
+    if (!defaults[key]) return message.reply("Esta chave não existe nas definições!");
+    const isDefault = !overrides[key] ? "\nEste é o valor original global desta definição." : "";
+    message.reply(`O valor de ${key} é ${settings[key]}${isDefault}`);
   } else {
     // Otherwise, the default action is to return the whole configuration;
     const array = [];
     Object.entries(settings).forEach(([key, value]) => {
       array.push(`${key}${" ".repeat(20 - key.length)}::  ${value}`); 
     });
-    await message.channel.send(`= Current Guild Settings =\n${array.join("\n")}`, {code: "asciidoc"});
+    await message.channel.send(`= Definições atuais deste servidor: =\n${array.join("\n")}`, {code: "asciidoc"});
   }
 };
 
@@ -80,12 +80,12 @@ exports.conf = {
   enabled: true,
   guildOnly: true,
   aliases: ["setting", "settings", "conf"],
-  permLevel: "Administrator"
+  permLevel: "MASTER"
 };
 
 exports.help = {
   name: "set",
-  category: "System",
-  description: "View or change settings for your server.",
+  category: "Sistema",
+  description: "Ver ou modificar as definições deste server..",
   usage: "set <view/get/edit> <key> <value>"
 };
