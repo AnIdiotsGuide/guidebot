c10c036a3e7c86ce90e0c503e9e08dec49889796
TheBigerGamer <39341546+TheBigerGamer@users.noreply.github.com>
2019-01-23 17:40:07 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const Discord = require("discord.js");
 const { promisify } = require("util");
 const readdir = promisify(require("fs").readdir);
 const Enmap = require("enmap");
+// Load up the xp from leveling system
+let xp = require("./xp.json");
 
 // This is your client. Some people call it `bot`, some people call it `self`,
 // some might call it `cootchie`. Either way, when you see `client.something`,
