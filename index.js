126443fca93f6053f74055de3a1c814f673e9f40
TheBigerGamer <39341546+TheBigerGamer@users.noreply.github.com>
2019-01-23 18:46:07 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ const Discord = require("discord.js");
 const { promisify } = require("util");
 const readdir = promisify(require("fs").readdir);
 const Enmap = require("enmap");
-const botconfig_colors = require("./botconfig_colors.json");
-// Load up the xp from leveling system
-let xp = require("./xp.json");
-let purple = botconfig_colors.purple;
 
 // This is your client. Some people call it `bot`, some people call it `self`,
 // some might call it `cootchie`. Either way, when you see `client.something`,
@@ -77,35 +73,6 @@ const init = async () => {
   }
   
   
-  // Here we load the xp system
-  let xpAdd = Math.floor(Math.random() * 7) + 8;
-  console.log(xpAdd);
-
-  if(!xp[message.author.id]){
-    xp[message.author.id] = {
-      xp: 0,
-      level: 1
-    };
-  }
-
-
-  let curxp = xp[message.author.id].xp;
-  let curlvl = xp[message.author.id].level;
-  let nxtLvl = xp[message.author.id].level * 300;
-  xp[message.author.id].xp =  curxp + xpAdd;
-  if(nxtLvl <= xp[message.author.id].xp){
-    xp[message.author.id].level = curlvl + 1;
-    let lvlup = new Discord.RichEmbed()
-    .setTitle("Subiu de Nível!")
-    .setColor(purple)
-    .addField("Novo Nível!", curlvl + 1);
-
-    message.channel.send(lvlup).then(msg => {msg.delete(5000)});
-  }
-  fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
-    if(err) console.log(err)
-  });
-
   // Here we login the client.
   client.login(client.config.token);
 
