c44d9ad7a6aa659b300f2ad8a8a0f89ae0d2dcba
Évelyne Lachance <eslachance@gmail.com>
2019-04-30 16:25:54 -0400

    Add setup.js, fix getSettings

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,5 @@ module.exports = async client => {
   client.logger.log(`${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
 
   // Make the bot "play the game" which is the help command with default prefix.
-  client.user.setActivity(`${client.config.defaultSettings.prefix}help`, {type: "PLAYING"});
+  client.user.setActivity(`${client.settings.get("default").prefix}help`, {type: "PLAYING"});
 };
--- modules/functions.js
+++ modules/functions.js
@@ -173,6 +173,7 @@ module.exports = (client) => {
   process.on("uncaughtException", (err) => {
     const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, "g"), "./");
     client.logger.error(`Uncaught Exception: ${errorMsg}`);
+    console.error(err);
     // Always best practice to let the code crash on uncaught exceptions. 
     // Because you should be catching them anyway.
     process.exit(1);
@@ -180,5 +181,6 @@ module.exports = (client) => {
 
   process.on("unhandledRejection", err => {
     client.logger.error(`Unhandled rejection: ${err}`);
+    console.error(err);
   });
 };
