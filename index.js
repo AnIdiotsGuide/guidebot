15dc3d7f131ed5252289fcb5ac824c0d2e5a2d73
Evelyne Lachance <eslachance@gmail.com>
2019-06-12 08:04:11 -0400

    Forgot to revert enmap path cuz testing

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Discord = require("discord.js");
 // We also load the rest of the things we need in this file:
 const { promisify } = require("util");
 const readdir = promisify(require("fs").readdir);
-const Enmap = require("../enmap");
+const Enmap = require("enmap");
 
 // This is your client. Some people call it `bot`, some people call it `self`,
 // some might call it `cootchie`. Either way, when you see `client.something`,
