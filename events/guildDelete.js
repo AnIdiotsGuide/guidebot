e3ac6d7801e8a70a3a3ed6fd21a7bb15697ed11f
Vetlix <31412314+Vetlix@users.noreply.github.com>
2020-02-22 19:25:24 +0200

    Check for outage on the guildDelete event.

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,7 +1,7 @@
 // This event executes when a new guild (server) is left.
 
 module.exports = (client, guild) => {
-  if (!guild.available) return; // If there is an outage, return.
+   if (!guild.available) return; // If there is an outage, return.
   
   client.logger.cmd(`[GUILD LEAVE] ${guild.name} (${guild.id}) removed the bot.`);
 
