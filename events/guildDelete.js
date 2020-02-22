fb503b3b0c61b52c6abc34eb4c9c12f8ab69c7d4
Vetlix <31412314+Vetlix@users.noreply.github.com>
2020-02-22 14:03:52 +0100

    Check for outage on the guildDelete event.

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,6 +1,8 @@
 // This event executes when a new guild (server) is left.
 
 module.exports = (client, guild) => {
+  if (!guild.available) return; // Check for an outage.
+  
   client.logger.cmd(`[GUILD LEAVE] ${guild.name} (${guild.id}) removed the bot.`);
 
   // If the settings Enmap contains any guild overrides, remove them.
