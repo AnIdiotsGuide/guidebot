871885517da57b79ab4a7c978dc8745d826ee6b2
York <YorkAARGH@users.noreply.github.com>
2020-05-02 15:53:08 +0100

    Update setup.js
    Forgot to update this, should fix #101

--- installer/setup.js
+++ installer/setup.js
@@ -2,7 +2,7 @@ const inquirer = require("inquirer");
 const Enmap = require("enmap");
 const fs = require("fs");
 
-let baseConfig = fs.readFileSync("./util/setup_base.txt", "utf8");
+let baseConfig = fs.readFileSync("./installer/setup_base.txt", "utf8");
 
 const defaultSettings = {
   "prefix": "~",
