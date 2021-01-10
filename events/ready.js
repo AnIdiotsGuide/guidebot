5fa90ea27f03c878b1bd6489106da65f85ebedbb
AndrewJWin <fastroute66@live.com>
2021-01-10 10:12:03 -0600

    Update ready event

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,17 @@
 module.exports = async client => {
   // Log that the bot is online.
   client.logger.log(`${client.user.tag}, ready to serve ${client.users.cache.size} users in ${client.guilds.cache.size} servers.`, "ready");
+  
+  // This loop ensures that client.application always contains up to date data
+  // about the app's status. This includes whether the bot is public or not,
+  // its description, owner(s), etc. Used for the dashboard amongs other things.
+  client.application = await client.fetchApplication();
+  if (client.owners.length < 1) client.application.team ? client.owners.push(...client.application.team.members.keys()) : client.owners.push(client.application.owner.id);
+  setInterval( async () => {
+    client.owners = [];
+    client.application = await client.fetchApplication();
+    client.application.team ? client.owners.push(...client.application.team.members.keys()) : client.owners.push(client.application.owner.id);
+  }, 60000);
 
   // Make the bot "play the game" which is the help command with default prefix.
   client.user.setActivity(`${client.settings.get("default").prefix}help`, {type: "PLAYING"});
