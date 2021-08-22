56ab96945770332780b277ec3ec1406b73c2ff2b
York <aarghgames@gmail.com>
2021-08-22 17:03:39 +0100

    Fixed events
    Removed `client` from `getSettings()` as it expects one variable, not two.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,7 +3,7 @@ const { getSettings } = require("../modules/functions.js");
 
 module.exports = (client, member) => {
   // Load the guild's settings
-  const settings = getSettings(client, member.guild);
+  const settings = getSettings(member.guild);
 
   // If welcome is off, don't proceed (don't welcome the user)
   if (settings.welcomeEnabled !== "true") return;
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -13,7 +13,7 @@ module.exports = async (client, message) => {
 
   // Grab the settings for this server from Enmap.
   // If there is no guild, get default conf (DMs)
-  const settings = message.settings = getSettings(client, message.guild);
+  const settings = message.settings = getSettings(message.guild);
 
   // Checks if the bot was mentioned, with no message after it, returns the prefix.
   const prefixMention = new RegExp(`^<@!?${client.user.id}>( |)$`);
