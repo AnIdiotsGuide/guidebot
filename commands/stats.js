5f7b36a0eccb461927b79c0039b63d5f0270b7eb
York <aarghgames@gmail.com>
2021-09-06 15:02:40 +0100

    Removed default params
    Both `stat` commands included redundant parameters, they have been removed.

--- commands/stats.js
+++ commands/stats.js
@@ -1,15 +1,7 @@
 const { version } = require("discord.js");
 const { codeBlock } = require("@discordjs/builders");
-const { DurationFormatter, TimeTypes } = require("@sapphire/time-utilities");
-const durationFormatter = new DurationFormatter({
-  [TimeTypes.Year]: { 1: "year", DEFAULT: "years" },
-  [TimeTypes.Month]: { 1: "month", DEFAULT: "months" },
-  [TimeTypes.Week]: { 1: "week", DEFAULT: "weeks" },
-  [TimeTypes.Day]: { 1: "day", DEFAULT: "days" },
-  [TimeTypes.Hour]: { 1: "hour", DEFAULT: "hours" },
-  [TimeTypes.Minute]: { 1: "minute", DEFAULT: "minutes" },
-  [TimeTypes.Second]: { 1: "second", DEFAULT: "seconds" }
-});
+const { DurationFormatter } = require("@sapphire/time-utilities");
+const durationFormatter = new DurationFormatter();
 
 exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
   const duration = durationFormatter.format(client.uptime);
