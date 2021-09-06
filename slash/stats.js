802d4819cb3f2cac5b3c49b9a838775a8a3f626b
York <aarghgames@gmail.com>
2021-09-06 12:33:11 +0100

    Dependency update
    Replaced the dead `moment`, and `moment-duration-format` dependencies with `@sapphire/time-utilities`.
    
    Replaced `chalk` with `colorette`.

--- slash/stats.js
+++ slash/stats.js
@@ -1,10 +1,18 @@
 const { version } = require("discord.js");
 const { codeBlock } = require("@discordjs/builders");
-const moment = require("moment");
-require("moment-duration-format");
+const { DurationFormatter, TimeTypes } = require("@sapphire/time-utilities");
+const durationFormatter = new DurationFormatter({
+  [TimeTypes.Year]: { 1: "year", DEFAULT: "years" },
+  [TimeTypes.Month]: { 1: "month", DEFAULT: "months" },
+  [TimeTypes.Week]: { 1: "week", DEFAULT: "weeks" },
+  [TimeTypes.Day]: { 1: "day", DEFAULT: "days" },
+  [TimeTypes.Hour]: { 1: "hour", DEFAULT: "hours" },
+  [TimeTypes.Minute]: { 1: "minute", DEFAULT: "minutes" },
+  [TimeTypes.Second]: { 1: "second", DEFAULT: "seconds" }
+});
 
 exports.run = async (client, interaction) => { // eslint-disable-line no-unused-vars
-  const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+  const duration = durationFormatter.format(client.uptime);
   const stats = codeBlock("asciidoc", `= STATISTICS =
 • Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
 • Uptime     :: ${duration}
