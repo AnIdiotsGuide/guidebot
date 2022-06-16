05a39591bb4ba78707e3877a2466471bd5ff8b3d
Doodesch <30825893+DooDesch@users.noreply.github.com>
2022-06-16 21:53:52 +0200

    Add role and category creater

---
+++ commands/addrole.js
@@ -0,0 +1,96 @@
+const { Permissions } = require('discord.js')
+const config = require('../config.js')
+const { settings } = require('../modules/settings.js')
+exports.run = async (client, message, args, level) => {
+    // eslint-disable-line no-unused-vars
+    const replying = settings.ensure(
+        message.guild.id,
+        config.defaultSettings
+    ).commandReply
+
+    if (!args || args.length < 1) return message.reply('Must provide a role name.')
+    const roleName = args[0]
+
+    // Check if rolename already exists
+    const roleExists = message.guild.roles.cache.find((r) => r.name === roleName)
+    if (roleExists) {
+        return message.reply('That role does already exist')
+    }
+
+    // Create role and give it to everybody that's mentioned
+    await message.guild.roles
+        .create({
+            name: roleName,
+            color: 'BLUE',
+            reason: 'Created for automatic channel and reactions',
+        })
+        .then((role) => {
+            message.mentions.members.forEach((member) => {
+                member.roles.add(role)
+            })
+        })
+        .catch(console.error)
+
+    // Get the just created role
+    const role = message.guild.roles.cache.find((r) => r.name === roleName)
+
+    // Check if channel already exists
+    const channel = message.guild.channels.cache.find((c) => c.name === roleName)
+    if (channel) {
+        return message.reply(
+            'Role category does already exist, skipping creating category and channels'
+        )
+    }
+
+    // Create category with channels, only accessable by role
+    const permissions = [
+        {
+            id: message.guild.roles.everyone,
+            deny: [Permissions.FLAGS.VIEW_CHANNEL],
+        },
+        {
+            id: role.id,
+            allow: [Permissions.FLAGS.VIEW_CHANNEL],
+        },
+    ]
+    message.guild.channels
+        .create(roleName, {
+            type: 'GUILD_CATEGORY',
+            permissionOverwrites: permissions,
+        })
+        .then((cat) => {
+            message.guild.channels.create('main', {
+                type: 'GUILD_TEXT',
+                parent: cat,
+                permissionOverwrites: permissions,
+            })
+            message.guild.channels.create('Main', {
+                type: 'GUILD_VOICE',
+                parent: cat,
+                permissionOverwrites: permissions,
+            })
+        })
+
+    // Add a final message
+    message.reply({
+        content: `The role \`${roleName}\` has been created ${
+            !channel ? 'and a category with channels has been added' : ''
+        }`,
+        allowedMentions: { repliedUser: replying === 'true' },
+    })
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 'Bot Admin',
+}
+
+exports.help = {
+    name: 'addrole',
+    category: 'System',
+    description:
+        'Adds a new role, creates a category for it as well as an reaction. When mentioning users, it will also assign the role.',
+    usage: 'addrole [command] (@users)',
+}
--- commands/help.js
+++ commands/help.js
@@ -5,65 +5,91 @@ command is also filtered by level, so if a user does not have access to
 a command, it is not shown to them. If a command name is given with the
 help command, its extended help is shown.
 */
-const { codeBlock } = require("@discordjs/builders");
-const { toProperCase } = require("../modules/functions.js");
+const { codeBlock } = require('@discordjs/builders')
+const { toProperCase } = require('../modules/functions.js')
 
 exports.run = (client, message, args, level) => {
-  // Grab the container from the client to reduce line length.
-  const { container } = client;
-  // If no specific command is called, show all filtered commands.
-  if (!args[0]) {
-    // Load guild settings (for prefixes and eventually per-guild tweaks)
-    const settings = message.settings;
-      
-    // Filter all commands by which are available for the user's level, using the <Collection>.filter() method.
-    const myCommands = message.guild ? container.commands.filter(cmd => container.levelCache[cmd.conf.permLevel] <= level) :
-      container.commands.filter(cmd => container.levelCache[cmd.conf.permLevel] <= level && cmd.conf.guildOnly !== true);
+    // Grab the container from the client to reduce line length.
+    const { container } = client
+    // If no specific command is called, show all filtered commands.
+    if (!args[0]) {
+        // Load guild settings (for prefixes and eventually per-guild tweaks)
+        const settings = message.settings
 
-    // Then we will filter the myCommands collection again to get the enabled commands.
-    const enabledCommands = myCommands.filter(cmd => cmd.conf.enabled);
+        // Filter all commands by which are available for the user's level, using the <Collection>.filter() method.
+        const myCommands = message.guild
+            ? container.commands.filter(
+                  (cmd) => container.levelCache[cmd.conf.permLevel] <= level
+              )
+            : container.commands.filter(
+                  (cmd) =>
+                      container.levelCache[cmd.conf.permLevel] <= level &&
+                      cmd.conf.guildOnly !== true
+              )
 
-    // Here we have to get the command names only, and we use that array to get the longest name.
-    const commandNames = [...enabledCommands.keys()];
+        // Then we will filter the myCommands collection again to get the enabled commands.
+        const enabledCommands = myCommands.filter((cmd) => cmd.conf.enabled)
 
-    // This make the help commands "aligned" in the output.
-    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+        // Here we have to get the command names only, and we use that array to get the longest name.
+        const commandNames = [...enabledCommands.keys()]
 
-    let currentCategory = "";
-    let output = `= Command List =\n[Use ${settings.prefix}help <commandname> for details]\n`;
-    const sorted = enabledCommands.sort((p, c) => p.help.category > c.help.category ? 1 : 
-      p.help.name > c.help.name && p.help.category === c.help.category ? 1 : -1 );
+        // This make the help commands "aligned" in the output.
+        const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0)
 
-    sorted.forEach( c => {
-      const cat = toProperCase(c.help.category);
-      if (currentCategory !== cat) {
-        output += `\u200b\n== ${cat} ==\n`;
-        currentCategory = cat;
-      }
-      output += `${settings.prefix}${c.help.name}${" ".repeat(longest - c.help.name.length)} :: ${c.help.description}\n`;
-    });
-    message.channel.send(codeBlock("asciidoc", output));
+        let currentCategory = ''
+        let output = `= Command List =\n[Use ${settings.prefix}help <commandname> for details]\n`
+        const sorted = enabledCommands.sort((p, c) =>
+            p.help.category > c.help.category
+                ? 1
+                : p.help.name > c.help.name && p.help.category === c.help.category
+                ? 1
+                : -1
+        )
 
-  } else {
-    // Show individual command's help.
-    let command = args[0];
-    if (container.commands.has(command) || container.commands.has(container.aliases.get(command))) {
-      command = container.commands.get(command) ?? container.commands.get(container.aliases.get(command));
-      if (level < container.levelCache[command.conf.permLevel]) return;
-      message.channel.send(codeBlock("asciidoc", `= ${command.help.name} = \n${command.help.description}\nusage:: ${command.help.usage}\naliases:: ${command.conf.aliases.join(", ")}`));
-    } else return message.channel.send("No command with that name, or alias exists.");
-  }};
+        sorted.forEach((c) => {
+            const cat = toProperCase(c.help.category)
+            if (currentCategory !== cat) {
+                output += `\u200b\n== ${cat} ==\n`
+                currentCategory = cat
+            }
+            output += `${settings.prefix}${c.help.name}${' '.repeat(
+                longest - c.help.name.length
+            )} :: ${c.help.description}\n`
+        })
+        message.channel.send(codeBlock('asciidoc', output))
+    } else {
+        // Show individual command's help.
+        let command = args[0]
+        if (
+            container.commands.has(command) ||
+            container.commands.has(container.aliases.get(command))
+        ) {
+            command =
+                container.commands.get(command) ??
+                container.commands.get(container.aliases.get(command))
+            if (level < container.levelCache[command.conf.permLevel]) return
+            message.channel.send(
+                codeBlock(
+                    'asciidoc',
+                    `= ${command.help.name} = \n${command.help.description}\nusage:: ${
+                        command.help.usage
+                    }\naliases:: ${command.conf.aliases.join(', ')}`
+                )
+            )
+        } else return message.channel.send('No command with that name, or alias exists.')
+    }
+}
 
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: ["h", "halp"],
-  permLevel: "User"
-};
+    enabled: true,
+    guildOnly: false,
+    aliases: ['h', 'halp'],
+    permLevel: 'User',
+}
 
 exports.help = {
-  name: "help",
-  category: "System",
-  description: "Displays all the available commands for your permission level.",
-  usage: "help [command]"
-};
+    name: 'help',
+    category: 'System',
+    description: 'Displays all the available commands for your permission level.',
+    usage: 'help [command]',
+}
